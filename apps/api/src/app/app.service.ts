e9a933e963d8147ac5126430549eaea61ff5bcea
theerebuss <39736248+theerebuss@users.noreply.github.com>
2025-09-02 13:26:00 +0200

    feat: add new service method

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -5,4 +5,7 @@ export class AppService {
   getData(): { message: string } {
     return ({ message: 'Hello API...' });
   }
+  getDataAlso(): { message: string } {
+    return ({ message: 'Hello API...' });
+  }
 }
